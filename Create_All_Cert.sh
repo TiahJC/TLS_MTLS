4f2c93a36ce9419afe89d289e689ae71b9706a22
Tiah Jye Chen <tiahjc@gmail.com>
2024-03-14 09:55:28 +0800

    Update Create_All_Cert.sh

--- Create_All_Cert.sh
+++ Create_All_Cert.sh
@@ -1,4 +1,4 @@
-openssl req -new x509 -days 1096 -config RootCA.cnf -keyout RootCA.key -out RootCA.pem
+openssl req -new -x509 -days 1096 -config RootCA.cnf -keyout RootCA.key -out RootCA.pem
 
 openssl genrsa -out SysA.key 4096
 
@@ -6,7 +6,7 @@ openssl req -new -config SysA.cnf -key SysA.key -out SysA.csr
 
 openssl x509 -req -extensions v3_ext -extfile ldap.cnf -passin "pass:changeme" -in SysA.csr -CA RootCA.pem -CAkey RootCA.key -days 1096 -CAcreateserial -out SysA.pem
 
-openssl req -new x509 -days 1096 -config RootCA.cnf -keyout RootCA2.key -out RootCA2.pem
+openssl req -new -x509 -days 1096 -config RootCA.cnf -keyout RootCA2.key -out RootCA2.pem
 
 openssl genrsa -out SysB.key 4096
 
